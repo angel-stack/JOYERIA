22446d1c2e43cf73e636daf1d0c33a263f0dc7c2
Jafeth Acendra <59936330+angel-stack@users.noreply.github.com>
2026-09-08 15:49:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const navbar = document.getElementById('navbar');
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('nav-glass');
       navbar.style.padding = '1rem 0';
     } else {
       navbar.classList.remove('nav-glass');
-      navbar.style.padding = '1.5rem 0';
     }
   });
 
